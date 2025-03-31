9a0b0168e92e89652056ddbf41b11f96dbd127c0
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2025-03-31 01:13:13 +0100

    ignore update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   eslint: {
+//     ignoreDuringBuilds: true, // Skips ESLint checks during next build
+//   },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true, // Skips ESLint checks during next build
   },
