5bb923f43c5db69933753c44057a539b44996c08
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2025-03-31 02:21:56 +0100

    Phase 1, Step 5: Added Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,26 @@
-import { ChakraProvider } from '@chakra-ui/react';
+// app/layout.tsx
+import Script from 'next/script';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="abc123" /> {/* From GSC */}
+      </head>
       <body>
-        <ChakraProvider>{children}</ChakraProvider>
+        {children}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-GHRJQPY730`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-GHRJQPY730');
+          `}
+        </Script>
       </body>
     </html>
   );
