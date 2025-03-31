c3b208ea8cbf3d12f767b072e078ebe26a600762
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2025-03-31 01:09:20 +0100

    Phase 1, Step 4: Ignore ESLint errors during build for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // Skips ESLint checks during next build
   },
 };
+
 module.exports = nextConfig;
