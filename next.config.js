ec1bb6c2999803a57ed31ab135c9e48ba8ee7e91
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2025-04-05 00:47:51 +0100

    total refactoring

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js
-require('dotenv').config({ path: './.env.local' });
-
-module.exports = {
-  env: {
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
-  },
-};
